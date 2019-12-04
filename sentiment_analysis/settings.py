cf0ceb9f83d0a4ed07baf3ef088881992eb34d1a
Kelly Deng <kelly.deng@stonybrook.edu>
2019-12-04 00:40:22 -0500

    Allow all hosts

--- sentiment_analysis/settings.py
+++ sentiment_analysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#0h+rbwx&lk(x%(u@o+en_z71e&69y92hcji3)3fh1%n4p#m%j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
